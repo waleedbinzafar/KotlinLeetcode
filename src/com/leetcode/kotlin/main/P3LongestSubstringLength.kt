608b03c151d46bfb2196bebb1e2cfbf17fbea5e5
waleedbinzafar <waleed.bin.zafar@gmail.com>
2024-07-08 18:15:10 +0500

    Leetcode Kotlin Practice

--- src/com/leetcode/kotlin/main/P3LongestSubstringLength.kt
+++ src/com/leetcode/kotlin/main/P3LongestSubstringLength.kt
@@ -1,5 +1,7 @@
 package com.leetcode.kotlin.main
 
+import kotlin.math.max
+
 class Solution2 {
     fun lengthOfLongestSubstring(s: String): Int {
         if (s.length<2){return s.length}
@@ -21,6 +23,20 @@ class Solution2 {
         }
         return substrings.maxOfOrNull { it.length }!!
     }
+
+    fun optimalLengthOfLongestSubstring(s: String): Int {
+        var maxLength = 0
+        var j = 0
+        val lastIndex = IntArray(128)
+        val charArray = s.toCharArray()
+        for (i in charArray.indices) {
+            j = max(j, lastIndex[charArray[i].code])
+            maxLength = max(maxLength, (i - j + 1))
+            lastIndex[charArray[i].code] = i + 1
+        }
+        return maxLength
+
+    }
 }
 
 fun main() {
@@ -28,5 +44,5 @@ fun main() {
     val string = "au"
     val solution = Solution2()
     println(string.length)
-    println(solution.lengthOfLongestSubstring(string))
+    println(solution.optimalLengthOfLongestSubstring(string))
 }
